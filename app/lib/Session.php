c21c4df9339deba576fbe4a10e38f84f843e5d81
Nababur Rahaman <nababur@users.noreply.github.com>
2024-09-24 00:53:36 +0600

    Update Session.php

--- app/lib/Session.php
+++ app/lib/Session.php
@@ -1,17 +1,7 @@
 <?php
 class Session{
   
-   public static function init(){
-    if (version_compare(phpversion(), '5.4.0', '<')) {
-          if (session_id() == '') {
-              session_start();
-          }
-      } else {
-          if (session_status() == PHP_SESSION_NONE) {
-              session_start();
-          }
-      }
-   }
+
    
    public static function set($key, $val){
     $_SESSION[$key] = $val;
